83d975eb2d93f8dc7268ccdb0a9aef10aa59604d
nathanWanderTech <lecongnha0901@gmail.com>
2022-11-07 13:08:26 -0500

    wip on create admin area

---
+++ Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+//using CongNhaBookStore.Models;
+using CongNhaBookStore.Models.ViewModels;
+
+namespace CongNhaBookStore.Area.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
